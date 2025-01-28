feebf19318b23d246c43c25fc8ae1d8ea153462c
TUSHAR <sabbir4hmed@outlook.com>
2025-01-28 22:23:48 +0600

    Initial commit

---
+++ app/src/main/java/com/sabbir/walton/mmitest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sabbir.walton.mmitest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.sabbir.walton.mmitest.TestActivities.PhoneDiagnostic;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, PhoneDiagnostic.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000); // 2 seconds delay
+    }
+}
