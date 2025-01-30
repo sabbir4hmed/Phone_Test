25c3ea287a5efbf1bedaf153bcaa3e5b888ba50c
TUSHAR <sabbir4hmed@outlook.com>
2025-01-30 13:03:03 +0600

    commit

--- app/src/main/java/com/sabbir/walton/mmitest/MainActivity.java
+++ app/src/main/java/com/sabbir/walton/mmitest/MainActivity.java
@@ -1,11 +1,16 @@
 package com.sabbir.walton.mmitest;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.sabbir.walton.mmitest.TestActivities.Phonediagnostic;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Allow layout to extend into the cutout area
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+
+        // Apply Immersive Mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_VISIBLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, PhoneDiagnostic.class);
+                Intent intent = new Intent(MainActivity.this, Phonediagnostic.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/sabbir/walton/mmitest/TestActivities/Phonediagnostic.java
+++ app/src/main/java/com/sabbir/walton/mmitest/TestActivities/Phonediagnostic.java
@@ -3,8 +3,10 @@ package com.sabbir.walton.mmitest.TestActivities;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -56,6 +58,22 @@ public class Phonediagnostic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_diagnostic);
+
+        // Allow layout to extend into the cutout area
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+
+        // Apply Immersive Mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_VISIBLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         initializeButtons();
         setupButtonListeners();
     }
